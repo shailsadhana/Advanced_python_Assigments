33c6e9149af060633f4123be45259d3662656fc9
shailendra Kumar <67969367+shailsadhana@users.noreply.github.com>
2020-08-09 21:08:27 +0530

    Add files via upload

---
+++ Weekend_Assigment_01.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+class Restaurant:
+    """method for Restaurant should store two attributes: a restaurant_name and a cuisine_type"""
+    def __init__(self,name,cuisine):
+        self.name=name
+        self.cuisine=cuisine
+        print("{name}, {cusisine}" )
+    def restaurant_name(self):
+        print(f"{self.name} is the name of Restaurant")
+
+    def cuisine_type(self):
+        print(f"{self.cuisine} is the type of cuissine")
+
+    Restaurant('rex','multi')
+    
+    a=Restaurant('rex','multi')
+    
+    
+    a.restaurant_name
+    print(f"{restaurant_name} is the name of restaurant")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
